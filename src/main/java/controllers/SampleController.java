07916fb151f4119a9c102e7c5b9fa32b4ce3a233
seth.ellison <seth.ellison@mis11.FDLIC.private.dig-inc.net>
2014-11-26 15:50:36 -0600

    - Split out templates to use includes - Updated to Spring Boot 1.2.0.RC2 - Added examples for using both @Value and the Environment API for dealing with property files.

--- src/main/java/controllers/SampleController.java
+++ src/main/java/controllers/SampleController.java
@@ -1,18 +1,44 @@
 package controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @EnableAutoConfiguration
+@PropertySource("classpath:/config/name.properties")
 public class SampleController {
 
+	@Value("${name}")
+	String name;
+	
+	@Value("${server.port}")
+	String port;
+	
+	@Autowired
+	Environment env;
+	
     @RequestMapping("/")
-    //@ResponseBody
     String home() {
+    	System.out.println("Default: Name: " + name + " Port: " + port);
+    	System.out.println("Env: Name: " + env.getProperty("name") + " Port: " + env.getProperty("server.port"));
+
+    	
         return "index";
+    }
+    
+    @RequestMapping("/learn_more")
+    String learnMore()
+    {
+    	System.out.println("Default: Name: " + name + " Port: " + port);
+    	System.out.println("Env: Name: " + env.getProperty("name") + " Port: " + env.getProperty("server.port"));
+
+    	return "learn_more";
     }
 
     public static void main(String[] args) throws Exception {
